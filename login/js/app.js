fd09e288be53c6b35a35c3ef2920e931c629d220
michael chuang <mchuang927@gmail.com>
2015-02-06 14:01:14 -0800

    complete login register forgotpassword ui

--- login/js/app.js
+++ login/js/app.js
@@ -1,83 +1,115 @@
 
 
- var LoginPanel = Backbone.View.extend({
+
+function inputDetect () {
+               $("#scrptRunBtn").text ("Script has been run.");
+               var inpsToMonitor = document.querySelectorAll (
+                   "form[name='user'] input[name^='inp']"
+               );
+               for (var J = inpsToMonitor.length - 1;  J >= 0;  --J) {
+                   inpsToMonitor[J].addEventListener ("change",    adjustStyling, false);
+                   inpsToMonitor[J].addEventListener ("keyup",     adjustStyling, false);
+                   inpsToMonitor[J].addEventListener ("focus",     adjustStyling, false);
+                   inpsToMonitor[J].addEventListener ("blur",      adjustStyling, false);
+                   inpsToMonitor[J].addEventListener ("mousedown", adjustStyling, false);
+                   //-- Initial update. note that IE support is NOT needed.
+                   var evt = document.createEvent ("HTMLEvents");
+                   evt.initEvent ("change", false, true);
+                   inpsToMonitor[J].dispatchEvent (evt);
+               }
+           };
+
+           function adjustStyling (zEvent) {
+               var inpVal  = zEvent.target.value;
+               var e;
+               if (inpVal  &&  inpVal.replace (/^\s+|\s+$/g, "") ){
+                   e = zEvent.target.parentNode.getElementsByClassName('placeHold')[0];
+                   if(e.className.indexOf('lock') == -1){
+                     e.className = e.className + " lock";
+                   }
+
+               }else{
+                   e = zEvent.target.parentNode.getElementsByClassName('placeHold')[0];
+                   e.className = "placeHold";
+               }
+           };
+
+ var AccountPanel = Backbone.View.extend({
 
           events: {
-              "click button#login" : "login",
-              "click a.forgot": "forgot",
-              "click a.register": "register",
+               "click form#login button#login" : "doLogin",
+               "click form#login a.forgot": "showForgot",
+               "click form#login a.register": "showRegister",
+
+               "click form#register button#register" : "doRegister",
+
+               "click form#reset button#sendEmail" : "doSendEmail",
+               "click form#reset button#checkToken" : "doCheckToken",
+               "click form#reset button#resetPwd" : "doResetPwd",
+
+               "click a.showLogin" : "showLogin",
           },
           initialize: function(){
-            console.log('LoginPanel has been initialized.');
-             _.bindAll(this, 'render', 'login');
+            console.log('AccountPanel has been initialized.');
+             //_.bindAll(this, 'render');
+             inputDetect();
             this.render();
           },
-          template: _.template( $("#loginPanel-template").html()),
+          template: _.template( $("#account-template").html()),
 
           render: function() {
-            this.$el.html( this.template );
+            this.$el.html( this.template({
+               logoImg:"",
+               logoText:'BETA',
+               footer:""
+            }));
             return this;
           },
-          login: function( evt ) {
-              var data = {
-                email:$('form#login').find( "input[name='inp-email']" ).val(),
-                password:$('form#login').find( "input[name='inp-pwd']" ).val()
-              }
-              alert('login attempt..');
+          doLogin: function( evt ) {
               $.ajax({
                 type: "POST",
                 url: "http://127.0.0.1:4000/api/v01/user/login",
-                data: data,
+                data: {
+                  email:$('form#login').find( "input[name='inp-email']" ).val(),
+                  password:$('form#login').find( "input[name='inp-pwd']" ).val()
+                },
                 success:function(ret){
                   window.localStorage.setItem('sessinToken', ret);
                   alert(window.localStorage.getItem('sessinToken'));
-                  $.ajaxSend(function(event, request) {
-                     var token = window.localStorage.getItem('sessinToken');//App.getAuthToken();
-                     if (token) {
-                        request.setRequestHeader("token", token);
-                     }
-                  });
                 }
               });
               evt.preventDefault( );
           },
-          forgot: function( evt ) {
+          showLogin:function(evt){
+            $('form#login').css('display','block');
+            $('form#register').css('display','none');
+            $('form#reset').css('display','none');
+          },
+          showForgot: function( evt ) {
               alert('forgot attempt');
+              $('form#reset').css('display','block');
+              $('form#login').css('display','none');
+              $('form#register').css('display','none');
           },
-          register: function(evt){
+          showRegister: function(evt){
 
-              $('.registerPanel').css('display','block');
-              $('.loginPanel').css('display','none');
-          }
-      });
-        var RegisterPanel = Backbone.View.extend({
-          events: {
-              "click button#register" : "register",
-              "click a.login" : "login",
+              $('form#register').css('display','block');
+              $('form#login').css('display','none');
+              $('form#reset').css('display','none');
           },
-          initialize: function(){
-            console.log('RegisterPanel has been initialized.');
-             _.bindAll(this, 'render', 'register');
-            this.render();
-          },
-          template: _.template( $("#register-template").html()),
-          render: function() {
-            this.$el.html( this.template );
-            return this;
-          },
-          register: function( evt ) {
+          doRegister: function( evt ) {
               alert('submit registration attempt');
-              var data = {
-                first_name:$('form#register').find( "input[name='inp-fname']" ).val(),
-                last_name:$('form#register').find( "input[name='inp-lname']" ).val(),
-                username:$('form#register').find( "input[name='inp-uname']" ).val(),
-                email:$('form#register').find( "input[name='inp-email']" ).val(),
-                password:$('form#register').find( "input[name='inp-pwd']" ).val()
-              }
+              var $f = $('form#register');
               $.ajax({
                 type: "POST",
                 url: "http://127.0.0.1:4000/api/v01/user",
-                data: data,
+                data: {
+                  first_name:$f.find( "input[name='inp-fname']" ).val(),
+                  last_name:$f.find( "input[name='inp-lname']" ).val(),
+                  username:$f.find( "input[name='inp-uname']" ).val(),
+                  email:$f.find( "input[name='inp-email']" ).val(),
+                  password:$f.find( "input[name='inp-pwd']" ).val()
+                },
                 success:function(ret){
                   //window.localStorage.setItem('sessinToken', ret);
                   alert(ret);
@@ -85,57 +117,31 @@
               });
               evt.preventDefault( );
           },
-          login:function(evt){
-            $('.loginPanel').css('display','block');
-            $('.registerPanel').css('display','none');
-          }
+          doSendEmail: function( evt ) {
+               //action
+              $('form#reset .reset-step-1').css('display','none');
+              $('form#reset .reset-step-2').css('display','block');
+              evt.preventDefault( );
+          },
+          doCheckToken: function( evt ) {
+            //action
+              $('form#reset .reset-step-2').css('display','none');
+              $('form#reset .reset-step-3').css('display','block');
+              evt.preventDefault( );
+          },
+          doResetPwd: function( evt ) {
+            //action
+              $('form#reset .reset-step-3').css('display','none');
+              $('form#reset .reset-success').css('display','block');
+              evt.preventDefault( );
+          },
       });
 
+  $(function() {
+     console.log('app start');
+        var accountPanel = new AccountPanel();
+        accountPanel.setElement( $( "#view-1" ) );
+        accountPanel.render( );
+        $('#view-1').append(accountPanel.$el);
 
-
-
-
-        function inputDetect () {
-            $("#scrptRunBtn").text ("Script has been run.");
-            var inpsToMonitor = document.querySelectorAll (
-                "form[name='user'] input[name^='inp']"
-            );
-            for (var J = inpsToMonitor.length - 1;  J >= 0;  --J) {
-                inpsToMonitor[J].addEventListener ("change",    adjustStyling, false);
-                inpsToMonitor[J].addEventListener ("keyup",     adjustStyling, false);
-                inpsToMonitor[J].addEventListener ("focus",     adjustStyling, false);
-                inpsToMonitor[J].addEventListener ("blur",      adjustStyling, false);
-                inpsToMonitor[J].addEventListener ("mousedown", adjustStyling, false);
-                //-- Initial update. note that IE support is NOT needed.
-                var evt = document.createEvent ("HTMLEvents");
-                evt.initEvent ("change", false, true);
-                inpsToMonitor[J].dispatchEvent (evt);
-            }
-        };
-
-        function adjustStyling (zEvent) {
-            var inpVal  = zEvent.target.value;
-            var e;
-            if (inpVal  &&  inpVal.replace (/^\s+|\s+$/g, "") ){
-                e = zEvent.target.parentNode.getElementsByClassName('placeHold')[0];
-                e.className = e.className + " lock";
-            }else{
-                e = zEvent.target.parentNode.getElementsByClassName('placeHold')[0];
-                e.className = "placeHold";
-            }
-        };
-
-$(function( ) {
-        //Backbone.Component.initialize( { "namespace" : Example } );
-
-        console.log('app start');
-        var login = new LoginPanel();
-        login.setElement( $( "#view-1" ) );
-        login.render( );
-        $('#view-1').append(login.$el);
-        var register = new RegisterPanel();
-        register.setElement( $( "#view-2" ) );
-        register.render( );
-        $('#view-2').append(register.$el);
-        inputDetect();
       });
